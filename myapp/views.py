69cb07358b75237c1a35a79056b821137b4cfebc
kouekamluc <luckevinkouekam@gmail.com>
2023-09-01 21:42:08 +0100

    2 initial commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request ,  'myapp/index.html')
+
+def contact(request):
+    return render(request , 'myapp/contact.html')
